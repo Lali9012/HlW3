fd185f97c182b8d0f868486c4ee2c6dc637949b4
Abdulahi HIrsi <141289773+Lali9012@users.noreply.github.com>
2026-04-25 16:41:29 -0700

    Update fifo.sv

--- fifo.sv
+++ fifo.sv
@@ -1,28 +1,42 @@
-/* FIFO buffer FWFT implementation for specified data and address
- * bus widths based on internal register file and FIFO controller.
- * Inputs: 1-bit rd removes head of buffer and 1-bit wr writes
- * w_data to the tail of the buffer.
- * Outputs: 1-bit empty and full indicate the status of the buffer
- * and r_data holds the value of the head of the buffer (unless empty).
+/* FIFO buffer where write width is twice read width.
+ * DATA_WIDTH = read width.
+ * w_data width = 2*DATA_WIDTH.
+ * Upper half is read first, then lower half.
  */
 module fifo #(parameter DATA_WIDTH=8, ADDR_WIDTH=4)
             (clk, reset, rd, wr, empty, full, w_data, r_data);
 
   input  logic clk, reset, rd, wr;
   output logic empty, full;
-  input  logic [DATA_WIDTH-1:0] w_data;
+  input  logic [2*DATA_WIDTH-1:0] w_data;
   output logic [DATA_WIDTH-1:0] r_data;
   
-  // signal declarations
   logic [ADDR_WIDTH-1:0] w_addr, r_addr;
   logic w_en;
-  
-  // enable write only when FIFO is not full
-  // or if reading and writing simultaneously
+  logic half_sel;
+
   assign w_en = wr & (~full | rd);
   
-  // instantiate FIFO controller and register file
-  fifo_ctrl #(ADDR_WIDTH) c_unit (.*);
-  reg_file #(DATA_WIDTH, ADDR_WIDTH) r_unit (.*);
+  fifo_ctrl #(ADDR_WIDTH) c_unit (
+    .clk(clk),
+    .reset(reset),
+    .rd(rd),
+    .wr(wr),
+    .empty(empty),
+    .full(full),
+    .w_addr(w_addr),
+    .r_addr(r_addr),
+    .half_sel(half_sel)
+  );
+
+  reg_file #(DATA_WIDTH, ADDR_WIDTH) r_unit (
+    .clk(clk),
+    .w_data(w_data),
+    .w_en(w_en),
+    .w_addr(w_addr),
+    .r_addr(r_addr),
+    .half_sel(half_sel),
+    .r_data(r_data)
+  );
   
-endmodule  // fifo
+endmodule
